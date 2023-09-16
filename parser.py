9c386b6a08f30a6b2201f99db533f7f564eab04f
Xichu Zhang <x472816@asia.fi.muni.cz>
2023-09-16 19:30:58 +0200

    new functions: get overall prices and usable areas

--- parser.py
+++ parser.py
@@ -2,19 +2,37 @@ import re
 from scrapper import remove_spaces
 
 
-def get_overall_price(raw_data):
+def get_numerical_value(raw_data, context):
     """
-    function extracts the overall price from the raw data
-    :param raw_data:
-    :return:
+    function extracts numerical value from raw data
+    :param raw_data: string of raw data
+    :param context: pattern, where to find the numerical value
+    :return: int, the target numerical value; None, if not found
     """
-    price_finder = re.search(r'Celková cena: (.*?) Kč', raw_data)
+    price_finder = re.search(context, raw_data)
     try:
         overall_price_str = price_finder.group(1)
         overall_price = int(remove_spaces(overall_price_str))
-        print(overall_price)
         return overall_price
-    except:
+    except AttributeError:
         print("overall price not found")
-    finally:
-        return None
+    return None
+
+
+def get_overall_price(raw_data):
+    """
+    function extracts the overall price
+    :param raw_data: string of raw data
+    :return: int, overall price
+    """
+    return get_numerical_value(raw_data, r'Celková cena: (.*?) Kč')
+
+
+def get_usable_area(raw_data):
+    """
+    function extracts the usable area
+    :param raw_data: string of raw data
+    :return: int, usable area
+    """
+    return get_numerical_value(raw_data, r'Užitná plocha: (.*?) m2')
+
